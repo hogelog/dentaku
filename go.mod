a2187cbeb27954be9af4fca8ba2f452d87a60f17
hogelog <konbu.komuro@gmail.com>
2020-06-23 19:03:48 +0900

    Use readline module

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/hogelog/dentaku
 
 go 1.14
 
-require github.com/PaesslerAG/gval v1.0.1
+require (
+	github.com/PaesslerAG/gval v1.0.1
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e
+	golang.org/x/sys v0.0.0-20200622214017-ed371f2e16b4 // indirect
+)
--- main.go
+++ main.go
@@ -1,22 +1,25 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 	"github.com/PaesslerAG/gval"
+	"github.com/chzyer/readline"
 )
 
 func main()  {
 	vars := map[string]interface{}{}
 
-	scanner := bufio.NewScanner(os.Stdin)
-	for true {
-		fmt.Print("> ")
-		if !scanner.Scan() {
+	rl, err := readline.New("> ")
+	if err != nil {
+		panic(err)
+	}
+	defer rl.Close()
+
+	for {
+		text, err := rl.Readline()
+		if err != nil {
 			break
 		}
-		text := scanner.Text()
 		value, err := gval.Evaluate(text, vars)
 		if err != nil {
 		    fmt.Println(err)
