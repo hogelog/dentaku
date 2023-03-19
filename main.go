de19236814abe84d3b1d5c9ef8a999f7b52e1288
hogelog <konbu.komuro@gmail.com>
2023-03-19 20:01:42 +0900

    Support history file

--- main.go
+++ main.go
@@ -5,12 +5,31 @@ import (
 	"github.com/PaesslerAG/gval"
 	"github.com/chzyer/readline"
 	"io"
+	"os"
+	"os/user"
+	"path/filepath"
 	"strings"
 )
 
 func main() {
+	usr, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	configDir := filepath.Join(usr.HomeDir, ".config", "dentaku")
+	err = os.MkdirAll(configDir, 0755)
+	if err != nil {
+		panic(err)
+	}
+
 	vars := map[string]interface{}{}
-	rl, err := readline.New("dentaku> ")
+
+	rlConfig := &readline.Config{
+		Prompt:            "dentaku> ",
+		HistoryFile:       filepath.Join(configDir, "history"),
+		HistorySearchFold: true,
+	}
+	rl, err := readline.NewEx(rlConfig)
 	if err != nil {
 		panic(err)
 	}
@@ -26,9 +45,7 @@ func main() {
 			continue
 		}
 
-		line = strings.ReplaceAll(line, ",", "")
-
-		value, err := gval.Evaluate(line, vars)
+		value, err := gval.Evaluate(normalize(line), vars)
 		if err != nil {
 			fmt.Println(err)
 			continue
@@ -36,4 +53,8 @@ func main() {
 
 		fmt.Println(value)
 	}
+}
+
+func normalize(line string) string {
+	return strings.ReplaceAll(line, ",", "")
 }
